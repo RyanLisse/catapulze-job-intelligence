bf0672f863622377af73cb11e85ee11f7d0a381f
Ryan Lisse <ryan.lisse@gmail.com>
2026-08-31 15:43:40 +0200

    feat(sources): add Flinter SSR connector (RJC-365)

--- packages/application/src/sources/index.ts
+++ packages/application/src/sources/index.ts
@@ -1,5 +1,6 @@
 import { bluetrail } from "./bluetrail";
 import type { SourceDefinition } from "./definition";
+import { flinter } from "./flinter";
 import { harveynash } from "./harveynash";
 import { hero } from "./hero";
 import { inhuurdesk } from "./inhuurdesk";
@@ -19,6 +20,7 @@ export { resolveTenderNedTestImportDays } from "./tenderned";
 /** Registry of every ingestable bron. Adding a source = one file in this folder + one line here. */
 export const SOURCES = {
   bluetrail,
+  flinter,
   harveynash,
   hero,
   inhuurdesk,
